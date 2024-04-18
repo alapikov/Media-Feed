fbffe5ccff2fc41255ba0468f209c42fed8f461a
Alexander Lapikov <a.d.lapikov@yandex.ru>
2024-04-18 14:05:05 +0300

    build 3 not working intersection observer

--- src/comps/FeedPage.tsx
+++ src/comps/FeedPage.tsx
@@ -1,28 +1,57 @@
-import React, {useState, useEffect, lazy, Suspense} from 'react';
-const PostItemLazy = lazy(() => import('./PostItem'));
+import React, {useState, useEffect, useRef, lazy, Suspense} from 'react';
+import {PostItemIsLoading} from './PostItem';
 import {apiBase} from '../data';
 import {Post} from '../types';
 const axios = require('axios').default;
 import '../styles/styles.styl';
 
 export const FeedPage: React.FC = () => {
+    const idx = useRef(2);
     const [posts, setPosts] = useState<Post[]>([]);
+    const [postsToRender, setPostsToRender] = useState<Post[]>([]);
+    const observerTarget = useRef(null);
 
     useEffect(() => {
-        axios.get(`${apiBase}/posts`).then((res: {data: Post[]}) => {
+        // prettier-ignore
+        axios.get(`${apiBase}/posts`)
+        .then((res: {data: Post[]}) => {
             setPosts(res.data);
+            setPostsToRender(res.data.slice(0, 2))
         });
     }, []);
 
+    
+
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+          if (entries[0].isIntersecting) {
+            function showMorePosts() {
+                const addedPosts = posts.slice(idx.current, idx.current + 2);
+                idx.current = idx.current + 2;
+                setPostsToRender([...postsToRender, ...addedPosts]);
+            }
+            showMorePosts();
+          }
+        });
+        observer.observe(observerTarget.current!);
+      }, []);
+    
+
     return (
         <>
-            {posts.map((post) => (
-                <Suspense fallback={<PostIsLoading />}>
-                    <PostItemLazy {...post} />;
+            {postsToRender.map((post) => (
+                <Suspense key={post.id} fallback={<PostItemIsLoading />}>
+                    {/* <div onClick={showMorePosts}> */}
+                    <PostItemLazy {...post} />
+                    {/* </div> */}
                 </Suspense>
             ))}
+            <div id='observerTarget' ref={observerTarget}></div>
         </>
     );
 };
 
-const PostIsLoading = () => <div>Пост грузится!</div>;
+const PostItemLazy = lazy(async () => {
+    await new Promise((resolve) => setTimeout(resolve, 1800));
+    return import('./PostItem');
+});
--- src/comps/PostItem.tsx
+++ src/comps/PostItem.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import '../styles/styles.styl';
 import Image from '../imgs/postImg.jpeg';
-import { Post } from '../types';
-import { apiBase } from '../data';
+import LoadingIcon from '../imgs/loadingIcon.jpg';
+import {Post} from '../types';
+import {apiBase} from '../data';
 import axios from 'axios';
 
 const PostItem = ({userId, id, title, body}: Post) => {
-    // axios.get(`${apiBase}/users/${userId}`)
-    // .then((res: {data: Post[]}) => {
-    //     console.log(res);
-    // })
+    axios.get(`${apiBase}/users/${userId}`).then((res: {data: Post[]}) => {});
 
     return (
         <div className='postItem'>
@@ -19,6 +17,14 @@ const PostItem = ({userId, id, title, body}: Post) => {
             <h4 className='postTitle'>{title}</h4>
             <p className='postText'>{body}</p>
             <p className='postAuthor'>{}</p>
+        </div>
+    );
+};
+
+export const PostItemIsLoading = () => {
+    return (
+        <div className='postItemIsLoading'>
+            <img src={LoadingIcon} alt=''></img>
         </div>
     );
 };
